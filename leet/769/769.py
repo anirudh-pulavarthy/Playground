73b95845dba2b58cf34dfd9eda4dc63540add954
Anirudh Pulavarthy <123797644+anirudh-pulavarthy@users.noreply.github.com>
2024-12-19 16:41:55 -0600

    Daily problem December 19

---
+++ leet/769/769.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        return 0
+
+if __name__ == "__main__":
+    arr = [4,3,2,1,0]
+
+    s = Solution()
+    res = s.maxChunksToSorted(arr)
+    print("Max chunks = ", res)
