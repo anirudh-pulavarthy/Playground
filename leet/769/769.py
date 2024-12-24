476a8884f2e144d2cdf4569f45969df5e3cc2791
Anirudh Pulavarthy <123797644+anirudh-pulavarthy@users.noreply.github.com>
2024-12-24 17:34:34 -0600

    Leet 769

--- leet/769/769.py
+++ leet/769/769.py
@@ -1,11 +1,33 @@
 from typing import List
 
 class Solution:
+    
     def maxChunksToSorted(self, arr: List[int]) -> int:
-        return 0
+        chunks = 0
+
+        start = 0
+        index = 0
+        st = []
+        for num in arr:
+            # If stack is empty and current number is index => it can be its own chunk
+            if not st and num == index:
+                start = index + 1
+                chunks += 1
+            else:
+                st.append(num)
+                test = set(range(start, index + 1))
+                test2 = set(st)
+                if test2 == test:
+                    chunks += 1
+                    st.clear()
+                    start = index + 1
+            
+            index += 1
+
+        return chunks
 
 if __name__ == "__main__":
-    arr = [4,3,2,1,0]
+    arr = [0,2,1,4,3]
 
     s = Solution()
     res = s.maxChunksToSorted(arr)
