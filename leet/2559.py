dae7a90b64a42f1419a16aa0491037aa201e37da
Anirudh Pulavarthy <123797644+anirudh-pulavarthy@users.noreply.github.com>
2025-01-02 13:20:29 -0600

    Daily problem Jan 2

---
+++ leet/2559.py
@@ -0,0 +1,18 @@
+class Solution:
+    def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
+        vowels = set("aeiou")
+
+        pre = []
+        count = 0
+        pre.append(count)
+        for word in words:
+            if word[0] in vowels and word[-1] in vowels:
+                count += 1
+            pre.append(count)
+
+        ans = []
+        for q in queries:
+            l, r = q
+            ans.append(pre[r + 1] - pre[l])
+
+        return ans
