333a8f0a79cb2beb1e87a530722aa55dd993ad5a
Anirudh Pulavarthy <123797644+anirudh-pulavarthy@users.noreply.github.com>
2025-02-14 02:53:06 -0600

    daily problem 14th Feb

---
+++ leet/1352.py
@@ -0,0 +1,24 @@
+class ProductOfNumbers:
+    
+    def __init__(self):
+        self.nums = [1]
+        
+
+    def add(self, num: int) -> None:
+        if num == 0:
+            self.nums = [1]
+        else:
+            self.nums.append(num * self.nums[-1])
+        
+
+    def getProduct(self, k: int) -> int:
+        if k > len(self.nums) - 1: return 0
+        else:
+            return self.nums[-1] // self.nums[len(self.nums) - k - 1]
+        
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
+# obj = ProductOfNumbers()
+# obj.add(num)
+# param_2 = obj.getProduct(k)
