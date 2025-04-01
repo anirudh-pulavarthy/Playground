41d892f1205684831bf1c3b897794a1409065f6d
Anirudh Pulavarthy <123797644+anirudh-pulavarthy@users.noreply.github.com>
2025-04-01 18:43:41 -0500

    Daily Problem Apr 1

---
+++ leet/2140.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        dp = [0] * len(questions)
+
+        for i in reversed(range(len(questions))):
+            ans_exclude = dp[i + 1] if (i + 1) < len(dp) else 0
+
+            next_solvable_idx = i + questions[i][1] + 1    # points to (i + brainpower(i) + 1)
+            next_ans = dp[next_solvable_idx] if next_solvable_idx < len(dp) else 0
+            ans_include = questions[i][0] + next_ans
+            dp[i] = max(ans_exclude, ans_include)
+
+        return max(dp)
